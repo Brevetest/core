775c18beb258c3b7a506bff2c9891ca4f542b1e8
Syed Azeez <syedazeez337@gmail.com>
2025-12-12 23:03:55 +0530

    Core setup for brevetest plugin
    Signed-off-by: Syed Azeez <syedazeez337@gmail.com>

---
+++ brevetest/src/main/java/com/brevetest/brevetest/BrevetestApplication.java
@@ -0,0 +1,13 @@
+package com.brevetest.brevetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrevetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrevetestApplication.class, args);
+	}
+
+}
